db1a5cd88f949b38391ce7998646c3383431e33d
Dowan Kim <dowankim@Dowanui-MacBook-Pro.local>
2019-04-14 22:06:26 -0400

    DONE functionally

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+var friendArray = [
+    {
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            5, 1, 4, 4, 5, 1, 2, 5, 4, 1
+        ]
+    },
+    {
+        name: "John",
+        photo: "https://media.apnarm.net.au/media/images/2017/10/27/imagev1731006674e03c04fb1c1b2c0030d2895-kne29a0sc2909sas5p2_ct677x380.jpg",
+        scores: [
+            3, 3, 1, 3, 5, 3, 2, 1, 4, 5
+        ]
+    },
+    {
+        name: "Jane",
+        photo: "https://www.readersdigest.ca/wp-content/uploads/sites/14/2017/11/facts-about-selfies-7.jpg",
+        scores: [
+            4, 3, 2, 4, 5, 5, 2, 4, 4, 3
+        ] 
+    }
+];
+
+module.exports = friendArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,51 @@
+var path = require('path');
+var friendArray = require('../data/friends');
+
+module.exports = function (app) {
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friendArray);
+    });
+
+    app.post("/api/friends", function (req, res) {
+
+        var soulmate = {
+          name: "",
+          photo: "",
+          difference: Infinity
+        };
+    
+    
+        // console.log(req.body);
+        // console.log(userScores);
+    
+        var userData = req.body;
+        var userScores = userData.scores;
+        
+    
+       
+        var totalDifference = 0;
+    
+        for (var i = 0; i < friendArray.length; i++) {
+        //   console.log(friendArray[i]);
+          totalDifference = 0;
+    
+          for (var j = 0; j < friendArray[i].scores[j]; j++); {
+    
+            totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friendArray[i].scores[j]))
+    
+            if (totalDifference <= soulmate.difference) {
+    
+              soulmate.name = friendArray[i].name;
+              soulmate.photo = friendArray[i].photo;
+              soulmate.difference = totalDifference;
+            }
+          }
+        }
+    
+        friendArray.push(userData);
+    
+        res.json(soulmate);
+      });
+    
+    }
